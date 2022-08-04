da697b74c34e456624ee559d38389bf1544b35fc
Sunil Chauhan <sunil121ml@gmail.com>
2022-08-04 12:47:10 +0530

    adding links to expenses and invoice routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Book-Keeper</h1>
+      <Link to={"/"}>
+        <h1>Book-Keeper</h1>
+      </Link>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to={"/invoices"}>Invoices</Link>
+        <Link to={"/expenses"}>Expenses</Link>
+      </nav>
     </div>
   );
 }
